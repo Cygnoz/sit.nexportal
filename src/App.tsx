13340284479485a1a0a396e33dfce2f3569249a6
AmanRasheed-cygnonex <amanrasheed@cygnonex.com>
2024-11-20 12:02:54 +0530

    Support Agent

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import DashboardPage from './pages/DashboardPage';
 import RegionHome from './modules/SaleArea&Region/Region/RegionHome';
 import AreaHome from './modules/SaleArea&Region/Area/AreaHome';
 import LeadHome from './modules/Customers/Lead/LeadHome';
+import SupportagentHome from './modules/Support/SupportAgent/SupportAgentHome';
 
 const App: React.FC = () => {
   const { role } = useRole(); // Access the role from context
@@ -21,6 +22,7 @@ const App: React.FC = () => {
             <Route path="dashboard" element={<DashboardPage />} />
             <Route path="regions" element={<RegionHome />} />
             <Route path="area" element={<AreaHome />} />
+            <Route path="support-agent" element={<SupportagentHome />} />
             <Route path="lead" element={<LeadHome />} />
             {/* Add more authenticated routes as needed */}
           </Route>
