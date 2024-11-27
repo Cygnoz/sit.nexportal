6246e895baf695de04f40292ec0b8e960c94f054
Thahanex <mohd.thaha@cygnonex.com>
2024-11-27 16:59:38 +0000

    log

--- router/Router.js
+++ router/Router.js
@@ -11,18 +11,18 @@ const { verifyToken } = require('../controller/authController/middleware');
 const ActivityLogGeneration = require('../controller/authController/activityLogController');
 
 
-router.post('/user',verifyToken,checkPermission('Super Admin','Add User'),userController.addUser,ActivityLogGeneration('user','Add User'))
+router.post('/user',verifyToken,checkPermission('Add User'),userController.addUser,ActivityLogGeneration('Add User'))
 
-router.get("/users",verifyToken,checkPermission('Super Admin','View Users'), userController.getAllUsers);
+router.get("/users",verifyToken,checkPermission('View User'), userController.getAllUsers);
 
-router.get("/user/:userId",verifyToken,checkPermission('Super Admin','View User'), userController.getUser);
+router.get("/user/:userId",verifyToken,checkPermission('View User'), userController.getUser);
 
-router.put("/user/:userId",verifyToken,checkPermission('Super Admin','Edit User'), userController.updateUser,ActivityLogGeneration('user','Edit User'));
+router.put("/user/:userId",verifyToken,checkPermission('Edit User'), userController.updateUser,ActivityLogGeneration('Edit User'));
 
-router.delete("/user/:userId",verifyToken,checkPermission('Super Admin','Delete User'), userController.deleteUser,ActivityLogGeneration('user','Delete User'));
+router.delete("/user/:userId",verifyToken,checkPermission('Delete User'), userController.deleteUser,ActivityLogGeneration('Delete User'));
 
 
-router.get('/get-activity-logs',verifyToken,checkPermission('Super Admin','Viewed Activity logs'),userController.getAllActivityLogs)
+router.get('/get-activity-logs',verifyToken,checkPermission('View logs'),userController.getAllActivityLogs)
 
 
 module.exports = router
