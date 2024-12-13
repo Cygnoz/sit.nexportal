54c9fcd038ed94c917f10de373992e6a969b1cd2
Thahanex <mohd.thaha@cygnonex.com>
2024-12-13 02:49:42 +0000

    error

--- router/Router.js
+++ router/Router.js
@@ -18,7 +18,7 @@ const { verifyToken } = require('../controller/authController/middleware');
 const ActivityLogGeneration = require('../controller/authController/activityLogController');
 
 // User
-router.post('/user',userController.addUser)
+router.post('/user',verifyToken,checkPermission('Add User'),userController.addUser,ActivityLogGeneration('Add User'))
 
 router.get("/users",verifyToken,checkPermission('View User'), userController.getAllUsers);
 
