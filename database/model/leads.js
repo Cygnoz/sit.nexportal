480ad62509bf753c10a344253e971b16e1b4473c
Thahanex <mohd.thaha@cygnonex.com>
2024-12-13 07:24:24 +0000

    new

--- database/model/leads.js
+++ database/model/leads.js
@@ -23,6 +23,7 @@ const leadSchema = new Schema({
   address: { type: String },
   city:{ type: String },
   state: { type: String },
+  country: {type : String },
 
   licenseType: { type: String },
   startDate: { type: String },
