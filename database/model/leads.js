b0a137d8d60dad3cbca945d62d54c759d7cbab16
unknown <amaljoy@cygnonex.com>
2025-01-07 09:44:42 +0530

    services

--- database/model/leads.js
+++ database/model/leads.js
@@ -5,9 +5,9 @@ const { Schema } = mongoose;
 
 const leadSchema = new Schema({
 
-  regionId:{ type: String },
-  areaId:{ type: String },
-  bdaId:{ type: String },
+  regionId:{ type: mongoose.Schema.Types.ObjectId, ref: 'Region'},
+  areaId:{ type: mongoose.Schema.Types.ObjectId, ref: 'Area'},
+  bdaId:{ type: mongoose.Schema.Types.ObjectId, ref: 'Bda'},
   customerId:{type:String},
   image:{ type: String },
   salutation :{ type: String },
@@ -18,15 +18,19 @@ const leadSchema = new Schema({
   phone: { type: String },
   website: { type: String },
   leadSource: { type: String },
-
+  regionManager: { type: mongoose.Schema.Types.ObjectId, ref: "RegionManager" },
+  areaManager: { type: mongoose.Schema.Types.ObjectId, ref: "AreaManager" },
+  
 
   address: { type: String },
   city:{ type: String },
   state: { type: String },
+  country : {type : String},
+  // licenseType: { type: String },
 
-  licenseType: { type: String },
   startDate: { type: String },
   endDate:{ type: String },
+  duration:{type:String},
 
   companyId: { type: String },
   companyName: { type: String },
@@ -36,10 +40,14 @@ const leadSchema = new Schema({
 
   licensorStatus:{ type: String },
   leadStatus:{ type: String },
-  customerStatus:{ type: String }
+  trialStatus:{type:String},
+  customerStatus:{ type: String },
+
+  
+  organizationId:{ type: String }
 
 },
-{ timestamps: true } 
+  { timestamps: true } 
 );
 
 const Lead = mongoose.model("Lead", leadSchema);
