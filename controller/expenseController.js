2cf91e736a9628919efc44218ed1110eca3190f9
unknown <amaljoy@cygnonex.com>
2025-02-14 17:23:50 +0530

    new

--- controller/expenseController.js
+++ controller/expenseController.js
@@ -219,33 +219,33 @@ exports.deleteExpense = async (req, res, next) => {
   
       const updateFields = { ...data, status: "Paid" };
 
-       const requestBody = {
-            organizationId: process.env.ORGANIZATION_ID,
-            expenseCategory: categoryName,
-            description: description,
-          };
-          // Generate JWT token
-          const token = jwt.sign(
-            {
-              organizationId: process.env.ORGANIZATION_ID,
-            },
-            process.env.NEX_JWT_SECRET,
-            { expiresIn: "12h" }
-          );
-          // https://billbizzapi.azure-api.net/staff/add-category-nexportal
-          // API call to external service
-          const response = await axios.post(
-            "https://billbizzapi.azure-api.net/staff/add-category-nexportal",
-            requestBody, // <-- requestBody should be passed as the second argument (data)
-            {
-              headers: {
-                Authorization: `Bearer ${token}`,
-                "Content-Type": "application/json",
-              },
-            }
-          );
+      //  const requestBody = {
+      //       organizationId: process.env.ORGANIZATION_ID,
+      //       expenseCategory: categoryName,
+      //       description: description,
+      //     };
+      //     // Generate JWT token
+      //     const token = jwt.sign(
+      //       {
+      //         organizationId: process.env.ORGANIZATION_ID,
+      //       },
+      //       process.env.NEX_JWT_SECRET,
+      //       { expiresIn: "12h" }
+      //     );
+      //     // https://billbizzapi.azure-api.net/staff/add-category-nexportal
+      //     // API call to external service
+      //     const response = await axios.post(
+      //       "https://billbizzapi.azure-api.net/staff/add-category-nexportal",
+      //       requestBody, // <-- requestBody should be passed as the second argument (data)
+      //       {
+      //         headers: {
+      //           Authorization: `Bearer ${token}`,
+      //           "Content-Type": "application/json",
+      //         },
+      //       }
+      //     );
   
-      const allAccounts = response.data;
+      // const allAccounts = response.data;
 
   
       // Update the expense with new values
